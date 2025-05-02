06cfa25e94861d5128393c1f8843275a8b3485da
gbart0198 <gbart0198@gmail.com>
2025-05-01 22:37:42 -0400

    expo app woowweoweo

---
+++ basketball-buddy/app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { Stack, Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+
+export default function NotFoundScreen() {
+    return (
+        <>
+            <Stack.Screen options={{ title: "Oops! Not Found." }} />
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                    Go back home
+                </Link>
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#25292E",
+    },
+    button: {
+        backgroundColor: "#007AFF",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    }
+})
