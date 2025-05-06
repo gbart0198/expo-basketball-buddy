342f142909a9d90f4db5048b1b3b73e0bc0ef70e
gbart0198 <gbart0198@gmail.com>
2025-05-06 15:30:23 -0400

    adding some layout and click register

--- basketball-buddy/app/(tracker)/tracker.tsx
+++ basketball-buddy/app/(tracker)/tracker.tsx
@@ -1,13 +1,40 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, TouchableOpacity, Platform } from "react-native";
 import { Image } from "expo-image";
 
 import PaddedSafeAreaView from "@/components/PaddedSafeAreaView";
+import { useRouter } from "expo-router";
 
 export default function ShotTrackerView() {
+    const router = useRouter();
+
+    const handleCourtPress = (event: any) => {
+        var locationX = 0;
+        var locationY = 0;
+        if (Platform.OS === "web") {
+            const nativeEvent = event.nativeEvent;
+
+            const rect = event.currentTarget.getBoundingClientRect();
+
+            locationX = nativeEvent.clientX - rect.left;
+            locationY = nativeEvent.clientY - rect.top;
+        } else {
+            const { locationX: x, locationY: y } = event.nativeEvent;
+            locationX = x;
+            locationY = y;
+
+        }
+        console.log(`Court pressed at X: ${locationX}, Y: ${locationY}`);
+        alert(`Court pressed at X: ${locationX}, Y: ${locationY}`);
+    }
     return (
         <PaddedSafeAreaView style={styles.screen}>
             <View style={styles.sessionContainer}>
-                <Text style={styles.sessionHeading}>SESSION STATS</Text>
+                <View style={styles.sessionHeaderContent}>
+                    <Text style={styles.sessionHeading}>SESSION STATS</Text>
+                    <Pressable style={styles.trackerPoint} onPress={() => router.back()}>
+                        <Text style={{ color: "white" }}>Cancel Session</Text>
+                    </Pressable>
+                </View>
                 <View style={styles.sessionContent}>
                     <View style={styles.sessionStatBox}>
                         <Text style={styles.sessionSubheading}>Made</Text>
@@ -17,32 +44,51 @@ export default function ShotTrackerView() {
                         <Text style={styles.sessionSubheading}>Attempted</Text>
                         <Text style={styles.sessionText}>22</Text>
                     </View>
-                    <Text style={styles.sessionPercentage}>54%</Text>
+                    <View style={styles.sessionStatBox}>
+                        <Text style={styles.sessionSubheading}>Shooting Percentage</Text>
+                        <Text style={styles.sessionText}>54%</Text>
+                    </View>
                 </View>
             </View>
             <View style={styles.courtContainer}>
-                <Image source={require("@/assets/images/court.jpg")} style={styles.courtImage} />
+                <Image source={require("@/assets/images/court.jpg")} style={styles.courtImage} contentFit="cover" />
+                <Pressable
+                    style={StyleSheet.absoluteFill}
+                    onPress={handleCourtPress}
+                />
             </View>
             <View style={styles.buttonsContainer}>
                 <Pressable style={styles.saveButton}>
                     <Text style={styles.buttonText}>Save</Text>
                 </Pressable>
-                <Pressable style={styles.endButton}>
+                <Pressable style={styles.endButton} onPress={() => router.back()}>
                     <Text style={styles.buttonText}>End Session</Text>
                 </Pressable>
             </View>
-        </PaddedSafeAreaView>
+        </PaddedSafeAreaView >
     );
 }
 
 const styles = StyleSheet.create({
     screen: {
-        backgroundColor: "#003952",
+        backgroundColor: "#76a6f5",
         flex: 1,
+    },
+    sessionHeaderContent: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 8,
+        justifyContent: "space-between"
+    },
+    trackerPoint: {
+        backgroundColor: "red",
+        padding: 8,
+        borderRadius: 5,
+        alignItems: "center",
     },
     sessionContainer: {
         flexDirection: "column",
-        flex: 1,
+        flex: 0.5,
         backgroundColor: "#032838",
         paddingHorizontal: 16,
         marginVertical: 0,
@@ -51,10 +97,11 @@ const styles = StyleSheet.create({
         borderRadius: 5,
     },
     sessionHeading: {
+        paddingVertical: 8,
         fontSize: 18,
         fontWeight: "bold",
         color: "white",
-        alignSelf: "center"
+        alignSelf: "flex-start"
     },
     sessionContent: {
         flexDirection: "row",
@@ -80,7 +127,6 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "bold",
         color: "white",
-        paddingBlockStart: 16,
     },
     buttonsContainer: {
         flexDirection: "row",
@@ -110,7 +156,7 @@ const styles = StyleSheet.create({
         color: "black",
     },
     courtContainer: {
-        flex: 5,
+        flex: 2,
         marginVertical: 8,
         marginHorizontal: 8,
         borderRadius: 5,
@@ -119,6 +165,6 @@ const styles = StyleSheet.create({
     courtImage: {
         width: "100%",
         height: "100%",
-    },
-    trackerPoint: {},
+        borderRadius: 5,
+    }
 });
--- basketball-buddy/app/_layout.tsx
+++ basketball-buddy/app/_layout.tsx
@@ -1,14 +1,34 @@
 // app/tracker/_layout.tsx
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import CustomHeader from "@/components/StackHeader";
+import { Header } from "@react-navigation/elements";
 
 export default function TrackerLayout() {
     return (
-        <Stack>
+        <Slot />
+    )
+}
+
+/*
+export default function TrackerLayout() {
+    return (
+        <Stack screenOptions={{
+            header: (props) => (
+                <CustomHeader
+                    title={props.route.name}
+                    showBackButton={props.back !== undefined}
+                />
+            ),
+        }}>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen
                 name="(tracker)/tracker"
-                options={{ headerShown: true, title: "Start Shooting!" }}
+                options={{
+                    headerShown: true,
+                    title: "Start Shooting!",
+                }}
             />
         </Stack>
     );
 }
+*/
